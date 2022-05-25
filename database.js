342b303d86f45014ed1bd6dcdb0fed3cc13bf553
emilasbringer <emil.asbringer@elev.ga.ntig.segit config --global user.email emil.asbringer@elev.ga.ntig.segit config --global user.email emil.asbringer@elev.ga.ntig.se>
2022-05-25 13:35:53 +0200

    🗃️ DB update

--- database.js
+++ database.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_DATABASE
+    database: process.env.DATABASE_DB
 });
 
 module.exports = pool;
